674d7abf0130ca33bed6a6381ce5adf72e1433c8
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-10-21 00:38:39 +0300

    add manual input, added error message.
    also fixed boxed shadow for mainboard.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+
 const mainBoard = document.querySelector('.mainboard');
 
-const btnSixteen = document.querySelector('.sixteen')
-
+//page load with 64 squares for coloring.
 appendSquares(8);
-
 let squareAfterCreation = document.querySelectorAll('.square');
+colorSquareOnMouseOver();
 
-squareAfterCreation.forEach(function (square) {
+// function for coloring the squares on mouse hoover.
+function colorSquareOnMouseOver () {
+    squareAfterCreation.forEach(function (square) {
     square.addEventListener('mouseover', colorSquare);
-});
+})};
 
+
+//variable declaration for all auto buttons. (will change the class going forward)
 const btns = document.querySelectorAll('.btn');
-
+//function for auto select with buttons.
 btns.forEach(function(button) {
     button.addEventListener('click', function(){
         if (button.classList.contains('sixteen')) {
@@ -24,13 +28,11 @@ btns.forEach(function(button) {
             appendSquares(16);
             squareAfterCreation = document.querySelectorAll('.square');
         }
-        squareAfterCreation.forEach(function (square) {
-         square.addEventListener('mouseover', colorSquare);
-        });
-    })
-    
+        colorSquareOnMouseOver ();
+    })  
 })
 
+//function for building the squares div.
 function appendSquares (duplicates) {
     mainBoard.innerHTML = ''
     for (let i=0;i < duplicates*duplicates; i++) {
@@ -42,6 +44,7 @@ function appendSquares (duplicates) {
     }
 }
 
+//function for coloring the squares.
 function colorSquare (e) {
     if (e.target.style.background == 'white') {
         e.target.style.background = 'black';
@@ -49,3 +52,31 @@ function colorSquare (e) {
     e.target.style.background = 'white';
     }
 }
+
+// variable for manual input
+const manualInputBtn = document.querySelector('.manualInputBtn');
+const manualInput = document.querySelector('#manualInput')
+
+// event listeners for click on apply button, and enter key on input manual.
+manualInputBtn.addEventListener('click', manualInputSubmit);
+manualInput.addEventListener('keydown' , function(e) {
+    if (e.keyCode === 13 || e.key === 'Enter') manualInputSubmit(); 
+});
+
+//function for manual input, user choose how many squares.
+function manualInputSubmit () {
+    if (parseInt(manualInput.value) > 100 || isNaN(parseInt(manualInput.value))) {
+        const error = document.createElement('div');
+        error.textContent = 'please enter a number (max 100)'
+        error.style.color = 'white';
+        manualInput.parentNode.insertBefore(error, manualInput);
+        setTimeout(() => {
+            manualInput.parentNode.removeChild(error); 
+        }, 1000);
+    } else {
+    const userManualSelect = parseInt(manualInput.value);
+    appendSquares(userManualSelect);
+    squareAfterCreation = document.querySelectorAll('.square')
+    colorSquareOnMouseOver();
+    }
+};

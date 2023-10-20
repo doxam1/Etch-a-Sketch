85d6e5b486a037897ec091d29cdcec1c6ea1082b
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-10-20 18:55:29 +0300

    squares colored white when mouse over
    second hoover coloring black again.
    added button for square amount.

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 const mainBoard = document.querySelector('.mainboard');
 
+const btnSixteen = document.querySelector('.sixteen')
+
+appendSquares(8);
+
+let squareAfterCreation = document.querySelectorAll('.square');
+
+squareAfterCreation.forEach(function (square) {
+    square.addEventListener('mouseover', colorSquare);
+});
+
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(button) {
+    button.addEventListener('click', function(){
+        if (button.classList.contains('sixteen')) {
+            appendSquares(4);
+            squareAfterCreation = document.querySelectorAll('.square');
+        } else if (button.classList.contains('sixtyfour')) {
+            appendSquares(8);
+            squareAfterCreation = document.querySelectorAll('.square');
+        } else if (button.classList.contains('twohundredfiftysix')) {
+            appendSquares(16);
+            squareAfterCreation = document.querySelectorAll('.square');
+        }
+        squareAfterCreation.forEach(function (square) {
+         square.addEventListener('mouseover', colorSquare);
+        });
+    })
+    
+})
+
 function appendSquares (duplicates) {
     mainBoard.innerHTML = ''
-    for (let i=1;i <= duplicates*duplicates; i++) {
+    for (let i=0;i < duplicates*duplicates; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         square.style.width = (400 / duplicates) + 'px'
@@ -11,4 +42,10 @@ function appendSquares (duplicates) {
     }
 }
 
-appendSquares(4);
+function colorSquare (e) {
+    if (e.target.style.background == 'white') {
+        e.target.style.background = 'black';
+    } else {
+    e.target.style.background = 'white';
+    }
+}
